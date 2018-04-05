d7440b3bcc9fb662f9fdaa7fdd3ab0fc32bf622c
AlexeyShpavda <alexeyshpavda@gmail.com>
2018-04-05 23:15:54 +0300

    Add a button to create an article and add an article display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,14 +2,20 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
     @article = Article.new
   end
+
   def create
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to article_path
+      redirect_to @article
     else
       render :new
     end
